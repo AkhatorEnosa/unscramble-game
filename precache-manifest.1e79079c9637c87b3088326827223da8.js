e5ac5c54325fac398f99f0dc1bc149d20e39a060
your <aosakhogba@gmail.com>
2020-05-25 14:38:15 +0100

    Updates

---
+++ precache-manifest.1e79079c9637c87b3088326827223da8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb331c9d4442a1a477c2f6773c366f8a",
+    "url": "/unscramble-game/index.html"
+  },
+  {
+    "revision": "0bbb7c43182a68823c74",
+    "url": "/unscramble-game/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "178df53d34907719cc95",
+    "url": "/unscramble-game/static/css/main.7071dbae.chunk.css"
+  },
+  {
+    "revision": "0bbb7c43182a68823c74",
+    "url": "/unscramble-game/static/js/2.011f7482.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unscramble-game/static/js/2.011f7482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178df53d34907719cc95",
+    "url": "/unscramble-game/static/js/main.0203685a.chunk.js"
+  },
+  {
+    "revision": "61d0e647fd717240b4b8",
+    "url": "/unscramble-game/static/js/runtime-main.7e37bbcd.js"
+  }
+]);
